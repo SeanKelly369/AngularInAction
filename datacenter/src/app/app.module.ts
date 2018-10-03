fa8296f1f4e84cc4c82e2704535d9a6c447f9820
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2018-10-03 19:58:32 +0100

    Add files via upload
    An update on the Angular in Action tutorial from chapter 4

--- datacenter/src/app/app.module.ts
+++ datacenter/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MetricComponent } from './metric/metric.component';
+import { NodesComponent } from './nodes/nodes.component';
+import { NodesRowComponent } from './nodes-row/nodes-row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DashboardComponent,
-    MetricComponent
+    MetricComponent,
+    NodesComponent,
+    NodesRowComponent
   ],
   imports: [
     BrowserModule,
--- datacenter/src/app/metric/metric.component.ts
+++ datacenter/src/app/metric/metric.component.ts
@@ -7,13 +7,32 @@ import { Component, Input } from '@angular/core';
 })
 export class MetricComponent {
 
-  @Input() title: string = '';
-  @Input() description: string = '';
-  @Input('used') value: number = 0;
-  @Input('available') max: number = 100;
+  @Input() title: string;
+  @Input() description: string;
+  @Input() value: number;
+  @Input() max: number;
+
+  @Input('used')
+  set value_(value: number) {
+    if (isNaN(value)) {
+      value = 0;
+    }
+    this.value = value;
+  }
+
+  get value_(): number { return this.value; }
+
+  @Input('available')
+  set max_(max: number) {
+    if (isNaN(max)) {
+      max = 100;
+    }
+    this.max = max;
+  }
+
+  get max_(): number { return this.max; }
 
   isDanger() {
     return this.value / this.max > 0.7;
   }
-
 }
