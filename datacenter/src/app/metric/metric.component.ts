eca3b05e865fa5e4117f59a76ea79f63e58dbb16
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2018-10-04 00:12:46 +0100

    Add files via upload

--- datacenter/src/app/metric/metric.component.ts
+++ datacenter/src/app/metric/metric.component.ts
@@ -1,36 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-metric',
   templateUrl: './metric.component.html',
-  styleUrls: ['./metric.component.css']
+  styleUrls: ['./metric.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MetricComponent {
+  @Input() value: number = 0;
+  @Input() max: number = 100;
 
-  @Input() title: string;
-  @Input() description: string;
-  @Input() value: number;
-  @Input() max: number;
+  // @Input('used')
+  // set value_(value: number) {
+  //   if (isNaN(value)) {
+  //     value = 0;
+  //   }
+  //   this.value = value;
+  // }
 
-  @Input('used')
-  set value_(value: number) {
-    if (isNaN(value)) {
-      value = 0;
+  // get value_(): number { return this.value; }
+
+  // @Input('available')
+  // set max_(max: number) {
+  //   if (isNaN(max)) {
+  //     max = 100;
+  //   }
+  //   this.max = max;
+  // }
+
+  // get max_(): number { return this.max; }
+
+  ngOnChanges(changes) {
+    if (changes.value && isNaN(changes.value.currentValue)) {
+      this.value = 0;
     }
-    this.value = value;
-  }
-
-  get value_(): number { return this.value; }
-
-  @Input('available')
-  set max_(max: number) {
-    if (isNaN(max)) {
-      max = 100;
+    if (changes.max && isNaN(changes.max.currentValue)) {
+      this.max = 0;
     }
-    this.max = max;
   }
-
-  get max_(): number { return this.max; }
 
   isDanger() {
     return this.value / this.max > 0.7;
